77bc1f667713fbe8b07ac3bef0fc222fec5c7407
NadinAhmed <nadinahmed316@gmail.com>
2026-05-19 11:18:18 -0700

    edit colors

---
+++ SportsZone/Colors/AppColors.swift
@@ -0,0 +1,24 @@
+//
+//  LightColors.swift
+//  SportsZone
+//
+//  Created by Nadin Ahmed on 19/05/2026.
+//
+
+import SwiftUI
+
+extension Color{
+    public static let primaryColor = UIColor(named: "PrimaryColor")
+
+    public static let secondaryColor = UIColor(named: "SecondryColor")
+
+    public static let accentColor = UIColor(named: "Accent")
+
+    public static let backgroundColor = UIColor(named: "Background")
+
+    public static let surfaceColor = UIColor(named: "Surface")
+
+    public static let textPrimaryColor = UIColor(named: "TextPrimary")
+
+    public static let textSecondaryColor = UIColor(named: "TextSecondry")
+}
