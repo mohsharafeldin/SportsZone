97e752ca267c89b60c07fe9d59b4aaf891bc1b9c
NadinAhmed <nadinahmed316@gmail.com>
2026-05-24 16:35:50 -0700

    connecting TeamDetailes with Api

---
+++ SportsZone/data/Model/Player.swift
@@ -0,0 +1,31 @@
+//
+//  Player.swift
+//  SportsZone
+//
+//  Created by Nadin Ahmed on 24/05/2026.
+//
+
+struct Player: Decodable {
+    let playerKey: Int
+    let playerImage: String
+    let playerName: String
+    let playerNumber: String
+    let playerType: String
+    let playerAge: String
+    let playerRating: String
+    let playerYellowCards: String
+    let playerRedCards: String
+    
+    enum CodingKeys: String, CodingKey{
+        case playerKey = "player_key"
+        case playerImage = "player_image"
+        case playerName = "player_name"
+        case playerNumber = "player_number"
+        case playerType = "player_type"
+        case playerAge = "player_age"
+        case playerRating = "player_rating"
+        case playerYellowCards = "player_yellow_cards"
+        case playerRedCards = "player_red_cards"
+    }
+
+}
--- SportsZone/data/Model/Team.swift
+++ SportsZone/data/Model/Team.swift
@@ -16,10 +16,15 @@ struct Team: Decodable {
     let teamKey: Int
     let teamName: String
     let teamLogo: String
+    let players : [Player]
+    let coaches : [Coach]
+    
 
     enum CodingKeys: String, CodingKey {
         case teamKey  = "team_key"
         case teamName = "team_name"
         case teamLogo = "team_logo"
+        case players = "players"
+        case coaches = "coaches"
     }
 }
