c340f915922eb6ee3b541baa6f800b6acf5eaf0c
mohsharafeldin <mohamedkingj17@gmail.com>
2026-06-01 09:39:05 +0300

    making cell for league lest and cell for favoritelesague

---
+++ SportsZone/Features/FavouriteLeagues/View/FavouriteLeagueCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavouriteLeagueCell.swift
+//  SportsZone
+//
+//  Created by mohamed sharaf on 01/06/2026.
+//
+
+import UIKit
+
+class FavouriteLeagueCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ SportsZone/Features/LeaguesList/View/LeagueCell.swift
@@ -0,0 +1,23 @@
+//
+//  LeagueCell.swift
+//  SportsZone
+//
+//  Created by mohamed sharaf on 01/06/2026.
+//
+
+import UIKit
+
+class LeagueCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
